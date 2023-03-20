95dfe6ab7b2d47239264bcb0ecf4118464333c67
Richard Le <levanthanh.ptit@gmail.com>
2023-03-20 22:53:16 +0700

    feat: mono repo script

---
+++ scripts/mono.bash
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+set -euo pipefail
+
+FLAG_PROJECT=${FLAG_PROJECT:-""}
+FLAG_PROFILE=${FLAG_PROFILE:-"default"}
+FLAG_RAW=()
+
+FLAG_CMD=$1
+shift 1
+
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+  -p | --project)
+    FLAG_PROJECT=$2
+    shift 2
+    ;;
+  --profile)
+    FLAG_PROFILE=$2
+    shift 2
+    ;;
+  --)
+    shift 1
+    FLAG_RAW=("$@")
+    ;;
+  *)
+    echo "Unrecognized argument: $1"
+    ;;
+  esac
+done
+
+readonly FLAG_PROJECT
+
+function init() {
+  cd $FLAG_PROJECT
+  aptos init $FLAG_RAW
+}
+
+function fund_contract() {
+  aptos account fund-with-faucet --account "$FLAG_PROFILE"
+}
+
+function publish() {
+  aptos move publish --named-addresses project="$FLAG_PROFILE"
+}
+
+case $FLAG_CMD in
+init)
+  init
+  ;;
+fund)
+  fund_contract
+  ;;
+publish)
+  publish
+  ;;
+esac
